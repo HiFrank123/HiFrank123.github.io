32a1743a464cc8ea4b33cc1d2195c528e0fc35a9
HiFrank123 <3601586354@qq.com>
2024-05-04 13:26:43 +0800

    Site updated: 2024-05-04 13:26:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/hello-world/"];function toRandomPost(){
+var posts=["2024/05/04/My-New-Post/","2024/05/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
